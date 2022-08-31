2188e089987bc2edb75430d10f0619981b152ca6
Renzo Kooi <renzo.kooi@gmail.com>
2022-08-31 10:33:34 +0200

    Update RegexpCreator.js
    Back 2 original

--- RegexpCreator.js
+++ RegexpCreator.js
@@ -1,11 +1,11 @@
 export default (regexStr, ...args) => {
-    const flags = args.length && Array.isArray(args.slice(-1)) ? args.pop() : [];
-    
-    return new RegExp(
-      (args.length &&
-        regexStr.raw.reduce( (a, v, i ) => a.concat(args[i-1] || ``).concat(v), ``) || 
-        regexStr.raw.join(``))
-        .split(`\n`)
-        .map( line => line.replace(/\s|\/\/.*$/g, ``).trim().replace(/@s/g, ` `) )
-        .join(``), flags.join(``) );
-  }
+  const flags = Array.isArray(args.slice(-1)) ? args.pop().join(``) : ``;
+
+  return new RegExp(
+    (args.length &&
+      regexStr.raw.reduce( (a, v, i ) => a.concat(args[i-1] || ``).concat(v), ``) || 
+      regexStr.raw.join(``))
+      .split(`\n`)
+      .map( line => line.replace(/\s|\/\/.*$/g, ``).trim().replace(/@s/g, ` `) )
+      .join(``), flags);
+}
