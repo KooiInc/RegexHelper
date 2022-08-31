f4e2dba62a61700e8d4f21c6d637cf8cd1c74ffa
Renzo Kooi <renzo.kooi@gmail.com>
2022-08-31 10:11:25 +0200

    Create RegexpCreator.js
    initial

---
+++ RegexpCreator.js
@@ -0,0 +1,11 @@
+export default (regexStr, ...args) => {
+    const flags = Array.isArray(args.slice(-1)) ? args.pop().join('') : ``;
+    
+    return new RegExp(
+      (args.length &&
+        regexStr.raw.reduce( (a, v, i ) => a.concat(args[i-1] || ``).concat(v), ``) || 
+        regexStr.raw.join(``))
+        .split(`\n`)
+        .map( line => line.replace(/\s|\/\/.*$/g, ``).trim().replace(/@s/g, ` `) )
+        .join(``), flags );
+  }
