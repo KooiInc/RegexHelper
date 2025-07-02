d1ea5360e8bdcf728ade13d0999915de1f165dca
Renzo Kooi <renzo.kooi@gmail.com>
2025-07-02 14:59:30 +0200

    small beer/re-version

--- RegexpCreator.js
+++ RegexpCreator.js
@@ -3,63 +3,55 @@ Object.defineProperties(instanceCreator, {escape: {value: escape4RE, enumerable:
 export default instanceCreator;
 
 function instanceCreator(regExStr, ...args) {
-  const {flags, cleanArgs} = maybeFlags(...args);
-  const initial = createIRegExpFromInput(regExStr, ...cleanArgs);
-
-  return createInstance(new RegExp(
-    initial.split(`\n`).map(line => cleanup(line)).join(``),
-    flags
-  ));
+  const {flags, cleanedArgs} = maybeFlags(...args);
+  const initial = createRegExpFromInput(regExStr, ...cleanedArgs);
+  return createInstance(new RegExp(initial.split(`\n`).map(line => cleanup(line)).join(``),flags));
 }
 
-function createIRegExpFromInput(regExStr, ...cleanArgs) {
-  return cleanArgs.length > 0
-    ? regExStr.raw.reduce((a, v, i) => a.concat(cleanArgs[i - 1] || ``).concat(v), ``)
-    : regExStr.raw.join(``);
+function createRegExpFromInput(regExStr, ...cleanedArgs) {
+  return !hasLength(cleanedArgs) ? regExStr.raw.join(``) :
+    regExStr.raw.reduce((a, v, i) => a.concat(cleanedArgs[i - 1] || ``).concat(v), ``);
 }
 
 function maybeFlags(...args) {
   const flags = cleanupFlags(getFlags(args.at(-1)));
-  return { flags, cleanArgs: flags.length > 0 ? args.slice(0, -1) : args };
+  return { flags, cleanedArgs: hasLength(flags) ? args.slice(0, -1) : args };
 }
 
-function getFlags(maybeFlags) {
-  return Array.isArray(maybeFlags) ? maybeFlags.join(``) : ``;
-}
+function getFlags(maybeFlags) {  return Array.isArray(maybeFlags) ? maybeFlags.join(``) : ``; }
 
 function createInstance(regExp) {
   const instance = new Proxy(Object.defineProperties({}, {
-    re: {
-      get() {
-        return regExp;
-      }, enumerable: false
-    },
+    re: { get() { return regExp; }, enumerable: false },
     toString: {value: () => regExp.toString(), enumerable: false},
     valueOf: {value: () => regExp, enumerable: false},
-    flags: {
-      value(flags) {
+    flags: { value(flags) {
         regExp = addFlags(flags, regExp);
-        return instance;
-      }, enumerable: false
-    },
+        return instance; }, enumerable: false },
   }), getterTrap(regExp));
-  
   return instance;
 }
 
+function hasLength(variable) { return variable?.length > 0; }
+
+function isOfType(variable, CTOR) { return variable?.constructor === CTOR; }
+
 function maybeProp(target, key, regExp) {
-  return {fromTarget: Reflect.get(target, key), fromInstance: Reflect.get(regExp, key)}
+  const fromRegExp = Reflect.get(regExp, key);
+  return {
+    fromInstance: Reflect.get(target, key),
+    fromRegExpMethod: isOfType(fromRegExp, Function) ? fromRegExp.bind(regExp) : regExp[key]
+  }
 }
 
 function getterTrap(regExp) {
   return {
     get(target, key) {
-      const {fromTarget, fromInstance} = maybeProp(target, key, regExp);
+      const {fromInstance, fromRegExpMethod} = maybeProp(target, key, regExp);
       
       switch (true) {
-        case !!fromTarget: return fromTarget;
-        case !!fromInstance && fromInstance.constructor === Function: return fromInstance.bind(target.re);
-        default: return regExp[key];
+        case !!fromInstance: return fromInstance;
+        default: return fromRegExpMethod;
       }
     },
   };
@@ -67,34 +59,29 @@ function getterTrap(regExp) {
 
 function cleanupFlags(flags, currentFlags) {
   currentFlags = (currentFlags ?? ``).replace(/^-r\|/, ``);
-  flags = currentFlags.concat(flags.constructor === String && flags.length ? flags : ``);
+  flags = currentFlags.concat(isOfType(flags, String) && hasLength(flags) ? flags : ``);
   return [...new Set([...flags])].join(``).replace(/[^dgimsuvy]/g, ``);
 }
 
 function addFlags(flags, re) {
   switch (true) {
+    case !isOfType(flags, String): return re;
     case flags.startsWith(`-r|`): return new RegExp(re.source, cleanupFlags(flags));
     case flags === `-r`: return new RegExp(re.source);
-    case flags.constructor === String && flags.length > 0: return new RegExp(re.source, cleanupFlags(flags, re.flags));
-    default: return re;
+    case hasLength(flags): return new RegExp(re.source, cleanupFlags(flags, re.flags));
   }
 }
 
 function escape4RE(reString) {
   switch (true) {
     case !!RegExp.escape: return RegExp.escape(reString);
-    default:
-      const first = `\\${reString.at(0)}`;
-      return first + reString.slice(1)
-        .replace(/\p{S}|\p{P}/gu, a => `\\${a}`)
-        .replace(/ /g, `\\x20`);
+    default:  return `\\x${reString.at(0)}` +
+      reString.slice(1).replace(/\p{S}|\p{P}/gu, a => `\\${a}`).replace(/ /g, `\\x20`);
   }
 }
 
 function cleanup(str) {
   return str
-    .replace(/\/\*(?:[^*]|\*+[^*\/])*\*+\/|(?<!:|\\\|')\/\/.*/g, ``)
-    .replace(/\s/g, ``)
-    .trim()
-    .replace(/<!([^>]\d+)>/g, (a, b) => String.fromCharCode(+b) ?? a);
+    .replace(/\/\*(?:[^*]|\*+[^*\/])*\*+\/|(?<!:|\\\|')\/\/.*/g, ``).replace(/\s/g, ``)
+    .trim().replace(/<!([^>]\d+)>/g, (a, b) => String.fromCharCode(+b) ?? a);
 }
