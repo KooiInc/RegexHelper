5335544fe8f9558ad89c2bf88312fea2646f5f2e
Renzo Kooi <renzo.kooi@gmail.com>
2025-07-08 14:02:53 +0200

    fix flags, add clone method

--- RegexpCreator.js
+++ RegexpCreator.js
@@ -1,9 +1,11 @@
+// v1.1.9
 export default Object.defineProperties(instanceCreator, {escape: {value: escape4RE, enumerable: true}});
 
 function instanceCreator(regExStr, ...args) {
   const {flags, cleanedArgs} = maybeFlags(...args);
   const initial = createRegExpFromInput(regExStr, ...cleanedArgs);
-  return createInstance(new RegExp(initial.split(`\n`).map(line => cleanup(line)).join(``),flags));
+  
+  return createInstance(new RegExp(initial.split(`\n`).map(line => cleanup(line)).join(``), flags));
 }
 
 function createRegExpFromInput(regExStr, ...cleanedArgs) {
@@ -19,14 +21,13 @@ function maybeFlags(...args) {
 function getFlags(maybeFlags) { return isOfType(maybeFlags, Array) ? maybeFlags.join(``) : ``; }
 
 function createInstance(regExp) {
-  const instance = new Proxy(Object.defineProperties({}, {
+  const instance = new Proxy( Object.defineProperties({}, {
     re: { get() { return regExp; }, enumerable: false },
     toString: {value: () => regExp.toString(), enumerable: false},
     valueOf: {value: () => regExp, enumerable: false},
-    flags: { value(flags) {
-        regExp = addFlags(flags, regExp);
-        return instance; }, enumerable: false },
-  }), getterTrap(regExp));
+    flags: { value(flags) { regExp = addFlags(flags, regExp); return instance; }, enumerable: false },
+    clone: { get() { return clone(instance); }, enumerable: false },
+  }), getterTrap(regExp) );
   return instance;
 }
 
@@ -54,18 +55,24 @@ function getterTrap(regExp) {
   };
 }
 
+function clone(instance) {
+  const source = instance.re.source;
+  const flags = hasLength(instance.re.flags) ? [...instance.re.flags] : [];
+  return instanceCreator`${source} ${flags}`;
+}
+
 function cleanupFlags(flags, currentFlags) {
   currentFlags = (currentFlags ?? ``).replace(/^-r\|/, ``);
   flags = currentFlags.concat(isOfType(flags, String) && hasLength(flags) ? flags : ``);
   return [...new Set([...flags])].join(``).replace(/[^dgimsuvy]/g, ``);
 }
 
-function addFlags(flags, re) {
+function addFlags(flags, regExp) {
   switch (true) {
-    case !isOfType(flags, String): return re;
-    case flags.startsWith(`-r|`): return new RegExp(re.source, cleanupFlags(flags));
-    case flags === `-r`: return new RegExp(re.source);
-    case hasLength(flags): return new RegExp(re.source, cleanupFlags(flags, re.flags));
+    case !isOfType(flags, String) || !hasLength(flags): return regExp;
+    case flags === `-r`: return regExp.compile(regExp.source);
+    case flags.startsWith(`-r|`): return regExp.compile(regExp.source, cleanupFlags(flags));
+    default: return regExp.compile(regExp.source, cleanupFlags(regExp.flags, flags));
   }
 }
 
@@ -79,6 +86,7 @@ function escape4RE(reString) {
 
 function cleanup(str) {
   return str
-    .replace(/\/\*(?:[^*]|\*+[^*\/])*\*+\/|(?<!:|\\\|')\/\/.*/g, ``).replace(/\s/g, ``)
+    .replace(/\/\*[^*]+\*\/|\/\/.+$/gm, ``)
+    .replace(/\s/g, ``)
     .trim().replace(/<!([^>]\d+)>/g, (a, b) => String.fromCharCode(+b) ?? a);
 }
